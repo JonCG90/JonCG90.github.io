3084995c3445f5a6a300842fbdc95cf039f7a36b
Jon Graham <JonCG90@gmail.com>
2020-03-29 22:47:56 +1300

    Move monte carlo chart into module

--- scripts/montecarlo.js
+++ scripts/montecarlo.js
@@ -1,48 +1,81 @@
-sampling = false;
+var monteCarloModule = (function() {
 
-function generateMonteCarloData(numSamples, minX, maxX) {
-
-	var data = [];
-
-	for (var i = 1; i <= numSamples; i++) {
-		data.push({
-			x: i,
-			y: i
-		});
-	}
-	return data;
-}
-
-var monteCarloChartData = {
-	datasets: [{
-		label: 'Line',
-		borderColor: 'rgb(255, 99, 132)',
-		backgroundColor: 'rgb(255, 99, 132)',
-		fill: false,
-		lineTension: 0,
-		data: generateMonteCarloData(numSamples, 0, 10),
-		type: 'line',
-		pointRadius: 3,
-		borderWidth: 3,
-	}] 
-};
-
-function addSample() {
-
-	if (sampling) {
-		numSamples++;
-
-		monteCarloChartData.datasets[0].data = generateMonteCarloData(numSamples, 0, 10);
-		window.monteCarlo.update();
-		window.setTimeout(addSample, 100);
-	}
-}
-
-document.getElementById('startSampling').addEventListener('click', function() {
-	sampling = true;
-	window.setTimeout(addSample, 100);
-});
-
-document.getElementById('stopSampling').addEventListener('click', function() {
 	sampling = false;
-});
+
+	function setup() {
+
+		var ctx = document.getElementById('monteCarloChart').getContext('2d');
+			window.monteCarlo = Chart.Scatter(ctx, {
+				data: monteCarloChartData,
+				options: {
+					animation: {
+		            	duration: 250,
+		        	},
+		        	legend: {
+		            	display: false,
+		        	},
+					tooltips: {
+						enabled: false
+					},
+					responsive: true,
+					stacked: false,
+					title: {
+						display: true,
+						text: 'Monte Carlo'
+					},
+				}
+			});
+	}
+
+	function generateMonteCarloData(numSamples, minX, maxX) {
+
+		var data = [];
+
+		for (var i = 1; i <= numSamples; i++) {
+			data.push({
+				x: i,
+				y: i
+			});
+		}
+		return data;
+	}
+
+	var monteCarloChartData = {
+		datasets: [{
+			label: 'Line',
+			borderColor: 'rgb(255, 99, 132)',
+			backgroundColor: 'rgb(255, 99, 132)',
+			fill: false,
+			lineTension: 0,
+			data: generateMonteCarloData(numSamples, 0, 10),
+			type: 'line',
+			pointRadius: 3,
+			borderWidth: 3,
+		}] 
+	};
+
+	function addSample() {
+
+		if (sampling) {
+			numSamples++;
+
+			monteCarloChartData.datasets[0].data = generateMonteCarloData(numSamples, 0, 10);
+			window.monteCarlo.update();
+			window.setTimeout(addSample, 100);
+		}
+	}
+
+	document.getElementById('startSampling').addEventListener('click', function() {
+		sampling = true;
+		window.setTimeout(addSample, 100);
+	});
+
+	document.getElementById('stopSampling').addEventListener('click', function() {
+		sampling = false;
+	});
+
+	return {
+	    setup: setup
+	 };
+
+})();
